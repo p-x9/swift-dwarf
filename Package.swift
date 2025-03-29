f0ca32b0d0afa004751b829b7832504dbf098b5d
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-03-29 17:28:45 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,21 +1,18 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "DWARF",
+    name: "swift-dwarf",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DWARF",
             targets: ["DWARF"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DWARF"),
+            name: "DWARF"
+        ),
         .testTarget(
             name: "DWARFTests",
             dependencies: ["DWARF"]
