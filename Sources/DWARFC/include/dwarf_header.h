39ac91455ad64ed167931e901e18e1ffe31cd57a
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-04-27 15:37:23 +0900

    Add c header of `dwarf_line_header` layout

--- Sources/DWARFC/include/dwarf_header.h
+++ Sources/DWARFC/include/dwarf_header.h
@@ -61,4 +61,52 @@ struct dwarf_aranges_header32_t {
     dwarf_ubyte segment_selector_size;
 };
 
+// .debug_line section
+// DWARF5 p154
+struct dwarf_line_header64_t {
+    struct dwarf_init_len64 unit_length;
+    dwarf_uhalf version;
+    dwarf_ubyte address_size;
+    dwarf_ubyte segment_selector_size;
+    uint64_t header_length; // 64/32
+    dwarf_ubyte minimum_instruction_length;
+    dwarf_ubyte maximum_operations_per_instruction;
+    dwarf_ubyte default_is_stmt;
+    dwarf_sbyte line_base;
+    dwarf_ubyte line_range;
+    dwarf_ubyte opcode_base;
+    uint64_t standard_opcode_lengths; // array of ubyte (size: opcode_base - 1)
+    dwarf_ubyte directory_entry_format_count;
+    uint64_t directory_entry_format; // sequence of ULEB128 pairs
+    uint64_t directories_count; // ULEB128
+    uint64_t directories; // sequence of directory names
+    dwarf_ubyte file_name_entry_format_count;
+    uint64_t file_name_entry_format; // sequence of ULEB128 pairs
+    uint64_t file_names_count; // ULEB128
+    uint64_t file_names; // sequence of file names
+};
+
+struct dwarf_line_header32_t {
+    struct dwarf_init_len32 unit_length;
+    dwarf_uhalf version;
+    dwarf_ubyte address_size;
+    dwarf_ubyte segment_selector_size;
+    uint32_t header_length;
+    dwarf_ubyte minimum_instruction_length;
+    dwarf_ubyte maximum_operations_per_instruction;
+    dwarf_ubyte default_is_stmt;
+    dwarf_sbyte line_base;
+    dwarf_ubyte line_range;
+    dwarf_ubyte opcode_base;
+    uint32_t standard_opcode_lengths; // array of ubyte (size: opcode_base - 1)
+    dwarf_ubyte directory_entry_format_count;
+    uint32_t directory_entry_format; // sequence of ULEB128 pairs
+    uint32_t directories_count; // ULEB128
+    uint32_t directories; // sequence of directory names
+    dwarf_ubyte file_name_entry_format_count;
+    uint32_t file_name_entry_format; // sequence of ULEB128 pairs
+    uint32_t file_names_count; // ULEB128
+    uint32_t file_names; // sequence of file names
+};
+
 #endif /* dwarf_header_h */
