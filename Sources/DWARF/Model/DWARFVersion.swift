b03645ce55b51e854e9d5f58b649d7168c262943
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-06-27 00:24:53 +0900

    Implemented `DWARFVersion`

---
+++ Sources/DWARF/Model/DWARFVersion.swift
@@ -0,0 +1,17 @@
+//
+//  DWARFVersion.swift
+//  swift-dwarf
+//
+//  Created by p-x9 on 2025/04/27
+//  
+//
+
+import Foundation
+
+public enum DWARFVersion: UInt16 {
+    case v1
+    case v2
+    case v3
+    case v4
+    case v5
+}
