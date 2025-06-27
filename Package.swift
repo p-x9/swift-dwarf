8d95bdc125f684b1992dd03d9ec72651ba6ba6bd
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-06-28 04:49:33 +0900

    Add `MachOKit` to dependencies

--- Package.swift
+++ Package.swift
@@ -4,17 +4,29 @@ import PackageDescription
 
 let package = Package(
     name: "swift-dwarf",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     products: [
         .library(
             name: "DWARF",
             targets: ["DWARF"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/p-x9/MachOKit.git", from: "0.35.0"),
+        .package(url: "https://github.com/p-x9/swift-fileio.git", from: "0.9.0")
+    ],
     targets: [
         .target(
             name: "DWARF",
             dependencies: [
-                "DWARFC"
+                "DWARFC",
+                .product(name: "MachOKit", package: "MachOKit"),
+                .product(name: "FileIO", package: "swift-fileio")
             ]
         ),
         .target(
