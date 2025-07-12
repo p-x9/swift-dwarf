d3c204621ebdceb231088f66ac3a2ed178911d62
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-07-12 18:22:01 +0900

    Add extension of `_FileIOProtocol`

---
+++ Sources/DWARF/Extension/_FileIOProtocol+.swift
@@ -0,0 +1,62 @@
+//
+//  _FileIOProtocol+.swift
+//  swift-dwarf
+//
+//  Created by p-x9 on 2025/06/29
+//  
+//
+
+import Foundation
+#if compiler(>=6.0) || (compiler(>=5.10) && hasFeature(AccessLevelOnImport))
+private import FileIO
+#else
+@_implementationOnly import FileIO
+#endif
+
+extension _FileIOProtocol {
+    /// (value, size)
+    func readULEB128(
+        baseOffset: UInt64
+    ) -> (UInt, Int) {
+        var value: UInt = 0
+        var shift: UInt = 0
+        var offset: Int = 0
+
+        var byte: UInt8 = 0
+
+        repeat {
+            byte = try! read(offset: numericCast(baseOffset) + offset)
+
+            value += UInt(byte & 0x7F) << shift
+            shift += 7
+            offset += 1
+        } while byte >= 128
+
+        return (value, offset)
+    }
+
+    /// (value, size)
+    func readSLEB128(
+        baseOffset: UInt64
+    ) -> (Int, Int) {
+        var value: Int = 0
+        var shift: UInt = 0
+        var offset: Int = 0
+
+        var byte: UInt8 = 0
+
+        repeat {
+            byte = try! read(offset: numericCast(baseOffset) + offset)
+
+            value += Int(byte & 0x7F) << shift
+            shift += 7
+            offset += 1
+        } while byte >= 128
+
+        if byte & 0x40 != 0 {
+            value |= -(1 << shift)
+        }
+
+        return (value, offset)
+    }
+}
