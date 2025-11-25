a64ffed3f79e23573ad11f82e8ec9132a8ab3f95
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-11-26 02:08:59 +0900

    Implemented directory-related models of dwarf5

---
+++ Sources/DWARF/Model/Line/DWARFDirectoryEntryFormat.swift
@@ -0,0 +1,44 @@
+//
+//  DWARFDirectoryEntryFormat.swift
+//  swift-dwarf
+//
+//  Created by p-x9 on 2025/11/25
+//  
+//
+
+import Foundation
+@testable @_spi(Support) import MachOKit
+import DWARFC
+
+public struct DWARFDirectoryEntryFormat {
+    public let type: DWARFLineContentType
+    public let format: DWARFAttributeFormat
+}
+
+extension DWARFDirectoryEntryFormat {
+    public var layoutSize: Int {
+        type.rawValue.uleb128Size + format.size
+    }
+}
+
+extension DWARFDirectoryEntryFormat {
+    public static func load(at offset: Int, in machO: MachOFile) throws -> Self? {
+        let offset = offset + machO.headerStartOffset
+
+        let (_type, size) = machO.fileHandle.readULEB128(
+            baseOffset: numericCast(offset)
+        )
+        guard let type = DWARFLineContentType(rawValue: numericCast(_type)) else {
+            return nil
+        }
+
+        let format: DWARFAttributeFormat = .load(
+            from: machO.fileHandle.ptr.advanced(by: offset + size)
+        )
+
+        return .init(
+            type: type,
+            format: format
+        )
+    }
+}
---
+++ Sources/DWARF/Model/Line/DWARFDirectoryEntryValue.swift
@@ -0,0 +1,49 @@
+//
+//  DWARFDirectoryEntryValue.swift
+//  swift-dwarf
+//
+//  Created by p-x9 on 2025/11/26
+//  
+//
+
+import Foundation
+@testable @_spi(Support) import MachOKit
+import DWARFC
+
+public struct DWARFDirectoryEntryValue {
+    public let type: DWARFLineContentType
+    public let value: DWARFAttributeValue
+
+    public let layoutSize: Int
+}
+
+extension DWARFDirectoryEntryValue {
+    public static func load(
+        at offset: Int,
+        for format: DWARFDirectoryEntryFormat,
+        in machO: MachOFile,
+        dwarfFormat: DWARFFormat,
+        addressSize: Int
+    ) throws -> Self? {
+        let offset = offset + machO.headerStartOffset
+
+        let value: DWARFAttributeValue? = .load(
+            at: offset,
+            from: machO,
+            as: format.format,
+            dwarfFormat: dwarfFormat,
+            addressSize: addressSize
+        )
+
+        guard let value else { return nil }
+
+        return .init(
+            type: format.type,
+            value: value,
+            layoutSize: value.size(
+                dwarfFormat: dwarfFormat,
+                addressSize: addressSize
+            )
+        )
+    }
+}
