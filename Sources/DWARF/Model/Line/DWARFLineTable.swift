7a2ee46ab79a28e3c291343397affd993edf02c5
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-11-29 01:57:58 +0900

    Implement process to obtain operation sequence of line table

--- Sources/DWARF/Model/Line/DWARFLineTable.swift
+++ Sources/DWARF/Model/Line/DWARFLineTable.swift
@@ -6,7 +6,8 @@
 //  
 //
 
-@_spi(Support) import MachOKit
+import Foundation
+@testable @_spi(Support) import MachOKit
 
 public struct DWARFLineTable {
     public let header: DWARFLineHeader
@@ -16,6 +17,68 @@ public struct DWARFLineTable {
 extension DWARFLineTable {
     public var layoutSize: Int {
         header.length + (header.format == ._64bit ? 12 : 4)
+    }
+}
+
+extension DWARFLineTable {
+    public struct Operations: Sequence {
+        private let header: DWARFLineHeader
+        public let data: Data
+
+        init(
+            header: DWARFLineHeader,
+            data: Data
+        ) {
+            self.header = header
+            self.data = data
+        }
+
+        public func makeIterator() -> Iterator {
+            .init(header: header, data: data)
+        }
+    }
+
+    public func oprations(for machO: MachOFile) throws -> Operations {
+        let data = try machO.fileHandle
+            .readData(
+                offset: machO.headerStartOffset + offset + header.layoutSize,
+                length: layoutSize - header.layoutSize
+            )
+        return .init(
+            header: header,
+            data: data
+        )
+    }
+}
+
+extension DWARFLineTable.Operations {
+    public struct Iterator: IteratorProtocol {
+        public typealias Element = DWARFLineOperation
+
+        private let header: DWARFLineHeader
+        private let data: Data
+        private var nextOffset: Int = 0
+
+        init(header: DWARFLineHeader, data: Data) {
+            self.header = header
+            self.data = data
+        }
+
+        public mutating func next() -> Element? {
+            guard nextOffset < data.count else { return nil }
+            var done: Bool = false
+            return data.withUnsafeBytes {
+                guard let basePointer = $0.baseAddress else { return nil }
+
+                return DWARFLineOperation.readNext(
+                    basePointer: basePointer.assumingMemoryBound(to: UInt8.self),
+                    operaionsSize: data.count,
+                    lineHeader: header,
+                    nextOffset: &nextOffset,
+                    done: &done
+                )
+            }
+        }
     }
 }
 
